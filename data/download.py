2c8837ef8a0f4c5f9ab3e7e99a7f8440e82b7e30
PhilippThoelke <philipp.thoelke@posteo.de>
2025-06-11 11:05:17 -0400

    Add data download script

---
+++ data/download.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+import gdown
+
+# The folder ID from your share link
+folder_url = "https://drive.google.com/drive/folders/1SNG7EK422o7qok2kQtUOJAetW3c9pNCK?usp=sharing"
+
+# Download all files in the folder into the current directory (recursive)
+gdown.download_folder(folder_url, output=(Path(__file__).parent.parent / "local_data").as_posix(), use_cookies=False)
